fad727012bf7dccfaf1254e1667a00a668f2dbdc
adi <adpascubv@yahoo.ro>
2013-10-11 08:47:30 +0300

    Add refresh Button

---
+++ Orar/src/main/java/ro/epb/orar/SyncActivity.java
@@ -0,0 +1,35 @@
+package ro.epb.orar;
+
+import android.app.Service;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class SyncActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View rootView = inflater.inflate(R.layout.activity_sync, null, false);
+        ListView orarListView = (ListView) rootView.findViewById(R.id.orarListView);
+        String[] data = {"asd", "dada"};
+        orarListView.setAdapter(new ArrayAdapter<String>(this,R.layout.orar_cell, R.id.left, data ));
+        setContentView(rootView);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.sync, menu);
+        return true;
+    }
+    
+}
